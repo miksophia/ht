421049d72a42014618c27ea48e34d6d27d691407
Sophia <miksofia@mail.ru>
2022-06-22 12:22:36 +0300

    first

---
+++ src/components/AppButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="cont-btn">
+    <slot/>
+  </button>
+</template>
+
+<style>
+  .cont-btn{
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 140%;
+    color: #FFFFFF;
+    background: #EB276D;
+    border-radius: 10px;
+    outline: none;
+    border: none;
+    padding: 15px 25px;
+    cursor: pointer;
+    width: fit-content;
+  }
+</style>
